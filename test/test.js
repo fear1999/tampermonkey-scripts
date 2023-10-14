68a9619b8ae2e22c2f113ad77e7ac1b9fe8fdd14
fear1999 <fear1999ero@gmail.com>
2023-10-14 17:02:01 +0800

    Add test

---
+++ test/test.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         Testing
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  test script. don't piublish
+// @author       fear1999
+// @match        *://*/*
+// ==/UserScript==
+
+function main() {
